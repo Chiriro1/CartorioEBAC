db47eeb108b5242428deac50ce1c8f758ee83d9e
Chiriro1 <150309074+Chiriro1@users.noreply.github.com>
2024-07-19 13:33:22 -0400

    Opção de saida

--- fabrica mathusao.cpp
+++ fabrica mathusao.cpp
@@ -122,6 +122,7 @@ int main()
 		printf("\t1 -  Registrar nomes\n");
 		printf("\t2 -  Consultar nomes\n");
 		printf("\t3 -  Deletar nomes\n"); // final do menu
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção:");
 	
 	
@@ -144,6 +145,11 @@ int main()
 			deletar();
 			break;
 			
+			case 4:
+			printf("Obrigado por utilizar o sistema")
+			return 0;
+			break;
+						
 			default:	
 			printf("Essa opção não está disponivel");
 			system("pause");
